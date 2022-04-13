fa73ecad2afbbb81bb846e53b65c10f8a7c55151
jalinden <97492113+jalinden@users.noreply.github.com>
2022-04-13 18:27:24 -0500

    Uploading completed file

---
+++ destructuring.js
@@ -0,0 +1,44 @@
+//1.
+8
+1846
+
+//2.
+{yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+//3.
+'Your name is Alejandro and you like purple'
+
+'Your name is Melissa and you like green'
+
+'Your name is undefined and you like green'
+
+//4.
+'Maya'
+'Marissa'
+'Chi'
+
+//5.
+'Raindrops on roses'
+'Whiskers on kittens'
+["Bright copper kettles",
+"warm woolen mittens",
+"Brown paper packages tied up with strings"]
+
+//6.
+[10,30,20]
+
+//7.
+const obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  const {a,b} = obj.numbers
+
+//8.
+const arr = [1,2]
+[arr[0], arr[1]] = [arr[1], arr[0]]
+
+//9.
+const raceResults = ([first,second,third,...rest]) => ({first,second,third,rest})
